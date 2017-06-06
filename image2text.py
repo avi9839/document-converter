da26a4c95eab7a351429656675bd71e5b07a3884
avi9839 <avipateldak@gmail.com>
2017-06-06 13:17:24 +0530

    image2text.py file updated!

--- image2text.py
+++ image2text.py
@@ -1,12 +1,12 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-im = Image.open("sample/content.png") # the second one
+im = Image.open("sample/image.jpg") # the second one
 im = im.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(im)
 im = enhancer.enhance(2)
 im = im.convert('1')
 im.save('sample/temp2.jpg')
-im = Image.open("sample/content.png")
-text = pytesseract.image_to_string(im)
-print(text)
+im = Image.open("Avinash.jpg")
+text = pytesseract.image_to_string(im,lang='eng')
+print(text.encode("utf-8"))
