db26816e1e136525bf610e0b1027583aebe3e778
avi9839 <avipateldak@gmail.com>
2017-06-06 12:15:35 +0530

    image to text file modifies and New folder crrated to keep the sample files

--- image2text.py
+++ image2text.py
@@ -1,11 +1,12 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-im = Image.open("content.png") # the second one
+im = Image.open("sample/content.png") # the second one
 im = im.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(im)
 im = enhancer.enhance(2)
 im = im.convert('1')
-im.save('temp2.jpg')
-text = pytesseract.image_to_string(Image.open('temp2.jpg'))
+im.save('sample/temp2.jpg')
+im = Image.open("sample/content.png")
+text = pytesseract.image_to_string(im)
 print(text)
--- image2textv.py
+++ image2textv.py
@@ -13,5 +13,5 @@ for y in range(img.size[1]):
             pix[x, y] = (255, 255, 255, 255)
 img.save('temp.jpg')
 
-file = pytesseract.image_to_string(Image.open('temp.jpg'))
+file = pytesseract.image_to_string(Image.open('sample/image.jpg'))
 print (file)
